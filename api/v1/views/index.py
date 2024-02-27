363d655da140c737090d20fe008a1c497eb2209b
Ahmed Hosny <ahmhosny2002@gmail.com>
2024-02-27 02:01:45 +0200

    Added initial API structure with /status endpoint

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Index file
+"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """Returns the status of the API"""
+    return jsonify({"status": "OK"})
